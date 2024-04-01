3ada65095555a4864174182cb316880a1e7abf65
M23CSA013 <m23csa013@iitj.ac.in>
2024-04-02 01:23:58 +0530

    adding feature-2 branch

--- Activity_1/feature.py
+++ Activity_1/feature.py
@@ -1,19 +1,31 @@
 import numpy as np
 
-def sigmoid(x):
-   return 1 / (1 + np.exp(-x))
+def relu(x):
+   return np.maximum(0, x)
+
+def leaky_relu(x, alpha=0.01):
+   return np.where(np.array(x) > 0, x, alpha*x)
+
+def tanh(x):
+   return np.tanh(x)
+
 random_values = [-3.5, -1.2, 0, 2.8, -4.1, 1.5, -0.7, 3.2, -2.4, 4.6]
 
 random_values = np.array(random_values)
 
-sigmoid_values = sigmoid(random_values)
+relu_values = relu(random_values)
+
+leaky_relu_values = leaky_relu(random_values)
+
+tanh_values = tanh(random_values)
 
 print("Random Values:")
 for value in random_values:
    print("{:.2f}".format(value), end=", ")
 print()
 
-print("Sigmoid Values:")
-for value in sigmoid_values:
-   print("{:.4f}".format(value), end=", ")
-print()
+print("Random Values:", random_values)
+print("ReLU Values:", relu_values)
+print("Leaky ReLU Values:", leaky_relu_values)
+print("Tanh Values:", tanh_values)
+
