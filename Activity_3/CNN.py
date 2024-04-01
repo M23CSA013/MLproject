8b32526893cc36875756ec6e91f99c9ecdbc22eb
M23CSA013 <m23csa013@iitj.ac.in>
2024-04-02 05:10:50 +0530

    Fix regression introduced in version2.0

--- Activity_3/CNN.py
+++ Activity_3/CNN.py
@@ -15,18 +15,16 @@ transform = transforms.Compose([
 trainset = datasets.USPS(root='./data', train=True, download=True, transform=transform)
 testset = datasets.USPS(root='./data', train=False, download=True, transform=transform)
 
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=16, shuffle=True)
-testloader = torch.utils.data.DataLoader(testset, batch_size=16, shuffle=False)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=32, shuffle=True)
+testloader = torch.utils.data.DataLoader(testset, batch_size=32, shuffle=False)
 
 class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
         self.conv1 = nn.Conv2d(1, 16, kernel_size=3, stride=1, padding=1)
         self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
-        self.conv3 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
-        self.conv4 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
         self.pool = nn.MaxPool2d(2, 2)
-        self.fc1 = nn.Linear(128 * 4 * 4, 128)
+        self.fc1 = nn.Linear(32 * 4 * 4, 128)
         self.fc2 = nn.Linear(128, 10)
 
     def forward(self, x):
@@ -34,7 +32,7 @@ class CNN(nn.Module):
         x = self.pool(F.relu(self.conv2(x)))
         x = self.pool(F.relu(self.conv3(x)))
         x = self.pool(F.relu(self.conv4(x)))
-        x = x.view(-1, 128 * 4 * 4)
+        x = x.view(-1, 32 * 4 * 4)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return x
@@ -75,6 +73,6 @@ def train_model(model, criterion, optimizer, train_loader, test_loader, epochs=1
 
 cnn_model = CNN()
 criterion = nn.CrossEntropyLoss()
-cnn_optimizer = optim.RMSprop(cnn_model.parameters(), lr=0.001)
+cnn_optimizer = optim.Adam(cnn_model.parameters(), lr=0.001)
 print("Training CNN...")
 train_model(cnn_model, criterion, cnn_optimizer, trainloader, testloader)
